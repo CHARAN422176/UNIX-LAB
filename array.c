a029d78b53121a73996556800d6f886845bcf9ef
CHARAN422176 <164168827+CHARAN422176@users.noreply.github.com>
2024-03-20 16:38:33 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5];
+    arr[10] = 42; // Accessing an array out of bounds
+    return 0;
+}
---
+++ null.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int *ptr = NULL;
+    *ptr = 5; // Dereferencing a NULL pointer
+    return 0;
+}
